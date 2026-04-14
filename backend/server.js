d9ca286901366520831c7fe26f626bfa87e8455b
Anish thakur <anishthakur2813@gmail.com>
2026-04-14 17:54:26 +0530

    fixed cors for vercel

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,10 @@ app.use(express.json());
 
 // Enable CORS
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: [
+    "http://localhost:3000",
+    "https://amazon-web-project-pq64wmday-anish5.vercel.app"
+  ],
   credentials: true,
 }));
 
